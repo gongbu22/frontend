2df5b50f05474ba72a1f8b11d727fcf6237b4f25
gongbu22 <pyujin0711@naver.com>
2024-11-06 13:51:31 +0900

    handlebars 패키지 설치

--- app.js
+++ app.js
@@ -2,25 +2,25 @@ let createError = require('http-errors');
 let express = require('express');
 let path = require('path');
 let port = 3000;
-//
-// // 프로메테우스 설치
-// const { collectDefaultMetrics, register } = require('prom-client');
-// // 기본 메트릭 수집
-// collectDefaultMetrics();
+
+// 프로메테우스 설치
+const { collectDefaultMetrics, register } = require('prom-client');
+// 기본 메트릭 수집
+collectDefaultMetrics();
 
 // // handlebars  추가
 // let session = require('express-session');
-// const handlebars = require('express-handlebars');
+const handlebars = require('express-handlebars');
 
 let indexRouter = require('./public/index.js');
 
 let app = express();
 
 // handlebars  설정
-// const hbs = handlebars.create({});
-// app.engine('handlebars', hbs.engine);
-// app.set('view engine', 'handlebars');
-// app.set('views', path.join(__dirname, 'public/handlebars'));
+const hbs = handlebars.create({});
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+app.set('views', path.join(__dirname, 'public/handlebars'));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
